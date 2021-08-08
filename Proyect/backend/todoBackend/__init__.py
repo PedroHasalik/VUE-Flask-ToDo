522feebe5174cfb5b7998d1dbd25085ba236987b
PedroHasalik <pedrohasa97@gmail.com>
2021-08-08 14:05:01 -0300

    added env variables for the db user and password

--- Proyect/backend/todoBackend/__init__.py
+++ Proyect/backend/todoBackend/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+
+dbUser = os.environ['DBUSER']
+dbPassword = os.environ['DBPASSWORD']
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://pedro:15432312@localhost/todos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://'+ dbUser + ':' + dbPassword + '@localhost/todos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.create_all()
